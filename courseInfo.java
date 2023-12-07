a79023ad214736a2defe35fe29ba350de2dc5309
huxiaohuanzhai <69295680+huxiaohuanzhai@users.noreply.github.com>
2023-12-07 14:44:34 -0500

    Update courseInfo.java
    update of CourseInfo

--- courseInfo.java
+++ courseInfo.java
@@ -1,4 +1,4 @@
-public class classInfo {
+public class CourseInfo {
     static int number = 0;
     int id;
     String name;
@@ -9,8 +9,9 @@ public class classInfo {
     String description;
     String professor;
 
-    classInfo( String name,String status, String location, String startTime, String endTime, String description,String professor){
+    CourseInfo( String name,String status, String location, String startTime, String endTime, String description,String professor){
         this.id = number;
+        this.name = name;
         this.status = status;
         this.location = location;
         this.startTime = startTime;
@@ -20,8 +21,20 @@ public class classInfo {
         number++;
     }
 
+    public String toString(){
+        String result = "";
+        result = result +"Course ID: "+Integer.toString(this.id)+"\n";
+        result = result +"Course Name: "+this.name+"\n";
+        result = result +"Instructor: "+this.professor+"\n";
+        result = result +"Status: "+this.status+"\n";
+        result = result +"Location: "+this.location+"\n";
+        result = result +"Start AT:" + this.endTime+"\n";
+        result = result +"End At:" + this.endTime+"\n";
+        result + result +"Info: "+this.description+"\n";
+        return result;
+    }
     public static void main(String[] agvs){
-        classInfo test = new classInfo( "Intro to Computer Science","open", "Bobst",  "8:00", "12:00", "A course", "Joanna")
-        System.out.println("hdsadas");
+        CourseInfo test = new CourseInfo( "Intro to Computer Science","open", "Bobst",  "8:00", "12:00", "A course", "Joanna");
+        System.out.println(test);
     }
 }
